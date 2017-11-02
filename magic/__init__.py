75ae347f149299d37363d1c6a29715906d397426
Ionel Cristian Mărieș <contact@ionelmc.ro>
2017-11-02 13:46:11 +0200

    Start converting to out-of-line binding.

--- magic/__init__.py
+++ magic/__init__.py
@@ -1,4 +1,5 @@
 from . import ffi
+from . import lib
 from . import flags
 
 __version__ = "0.4.0"
@@ -19,10 +20,10 @@ class Magic(object):
       ffi.close(self.cookie)
     except Exception as exception:
       raise
-  
+
   def __enter__(self):
     return self
-  
+
   def __exit__(self, type, value, traceback):
     del self
 
@@ -48,3 +49,58 @@ class Magic(object):
   @handle_bytes
   def from_buffer(self, value):
     return ffi.buffer(self.cookie, value)
+
+def handle_null_exception(function):
+  def wrapper(cookie, *args, **kwargs):
+    response = function(cookie, *args, **kwargs)
+    if response == ffi.NULL:
+      message = error(cookie)
+      raise ValueError(message)
+    else:
+      return ffi.string(response)
+  return wrapper
+
+def version():
+  return lib.magic_version()
+
+def set_flags(cookie, flags):
+  status = lib.magic_setflags(cookie, flags)
+  if status != 0:
+    message = error(cookie)
+    raise ValueError(message)
+  else:
+    return True
+
+def error(cookie):
+  message = lib.magic_error(cookie)
+  return ffi.string(message)
+
+def open(flags):
+  cookie = lib.magic_open(flags)
+  if cookie == ffi.NULL:
+    message = error(cookie)
+    raise RuntimeError(message)
+  else:
+    return cookie
+
+def close(cookie):
+  closed = lib.magic_close(cookie)
+  return True
+
+def load(cookie, path=ffi.NULL):
+  status = lib.magic_load(cookie, path)
+  if status != 0:
+    message = error(cookie)
+    raise ValueError(message)
+  else:
+    return True
+
+@handle_null_exception
+def file(cookie, path):
+  mimetype = lib.magic_file(cookie, path)
+  return mimetype
+
+@handle_null_exception
+def buffer(cookie, value):
+  mimetype = lib.magic_buffer(cookie, value, len(value))
+  return mimetype
--- magic/ffi.py
+++ magic/ffi.py
@@ -1,5 +1,4 @@
 from cffi import FFI
-from . import flags
 
 
 ffi = FFI()
@@ -21,62 +20,9 @@ ffi.cdef("""
   int magic_list(magic_t, const char *);
   int magic_errno(magic_t);
 """)
+ffi.set_source("magic.lib", "#include <magic.h>")
+#
+# magic = ffi.verify("#include <magic.h>",
+#                    libraries=["magic"],
+#                    ext_package="magic")
 
-magic = ffi.verify("#include <magic.h>",
-                   libraries=["magic"],
-                   ext_package="magic")
-
-def handle_null_exception(function):
-  def wrapper(cookie, *args, **kwargs):
-    response = function(cookie, *args, **kwargs)
-    if response == ffi.NULL:
-      message = error(cookie)
-      raise ValueError(message)
-    else:
-      return ffi.string(response)
-  return wrapper
-
-def version():
-  return magic.magic_version()
-
-def set_flags(cookie, flags):
-  status = magic.magic_setflags(cookie, flags)
-  if status != 0:
-    message = error(cookie)
-    raise ValueError(message)
-  else:
-    return True 
-
-def error(cookie):
-  message = magic.magic_error(cookie)
-  return ffi.string(message)
-
-def open(flags):
-  cookie = magic.magic_open(flags)
-  if cookie == ffi.NULL:
-    message = error(cookie)
-    raise RuntimeError(message)
-  else:
-    return cookie 
-
-def close(cookie):
-  closed = magic.magic_close(cookie)
-  return True
-
-def load(cookie, path=ffi.NULL):
-  status = magic.magic_load(cookie, path)
-  if status != 0:
-    message = error(cookie)
-    raise ValueError(message)
-  else:
-    return True
-
-@handle_null_exception
-def file(cookie, path):
-  mimetype = magic.magic_file(cookie, path)
-  return mimetype
-
-@handle_null_exception
-def buffer(cookie, value):
-  mimetype = magic.magic_buffer(cookie, value, len(value))
-  return mimetype
